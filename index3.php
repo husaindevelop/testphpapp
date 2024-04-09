fc950ddf16c13e3ceec121c9277246673319d45c
Ahamed Husain <husaindevelop@gmail.com>
2024-04-09 12:04:16 +0530

    newest commit

--- index3.php
+++ index3.php
@@ -7,7 +7,37 @@
         <link href="css/style.css" rel="stylesheet">
     </head>
     <body>
-        <?php 
+        <?php
+        
+        
+use PHPUnit\Framework\TestCase;
+
+
+class CalculatorTest extends TestCase
+{
+    public function testAdd()
+    {
+        $calculator = new Calculator();
+        $result = $calculator->add(2, 3);
+        $this->assertEquals(5, $result);
+    }
+
+    public function testSubtract()
+    {
+        $calculator = new Calculator();
+        $result = $calculator->subtract(5, 3);
+        $this->assertEquals(2, $result);
+    }
+}
+
+
+
+
+
+
+
+
+
         
         echo "You know what is going on".strtoupper ("Hello");
         
